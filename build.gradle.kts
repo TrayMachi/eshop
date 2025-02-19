5b69f9b018f24a1772000da986144f2bfc3d2d2d
Tristan A. <tristanagraa@gmail.com>
2025-02-20 03:30:59 +0700

    feat: sonarqube workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -22,6 +23,14 @@ configurations {
 
 repositories {
     mavenCentral()
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "TrayMachi_eshop")
+    property("sonar.organization", "traymachi")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
 
 val seleniumJavaVersion = "4.14.1"
