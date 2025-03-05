d91bc18360f33cd89ecc6ce625d43d6efc3beb42
Tristan A. <tristanagraa@gmail.com>
2025-03-06 03:06:39 +0700

    [RED] Add tests for Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -10,6 +10,9 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import id.ac.ui.cs.advprog.eshop.order.model.Order;
+import id.ac.ui.cs.advprog.eshop.product.model.Product;
+
 class OrderTest {
     private List<Product> products;
     
@@ -17,11 +20,11 @@ class OrderTest {
     void setUp() {
         this.products = new ArrayList<>();
         Product product1 = new Product();
-        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product1.setProductName("Sampo Cap Bambang");
         product1.setProductQuantity(2);
         Product product2 = new Product();
-        product2.setProductId("123e4567-e89b-12d3-a456-556642440001");
+        product2.setId("123e4567-e89b-12d3-a456-556642440001");
         product2.setProductName("Sabun Cap Usep");
         product2.setProductQuantity(1);
         this.products.add(product1);
@@ -43,11 +46,11 @@ class OrderTest {
 
         assertSame(this.products, order.getProducts());
         assertEquals(2, order.getProducts().size());
-        assertEquals("Sampo Cap Bambang", order.getOrderId().get(0).getProductName());
-        assertEquals("Sabun Cap Usep", order.getOrderId().get(1).getProductName());
+        assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductName());
+        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());
 
         assertEquals("123e4567-e89b-12d3-a456-556642440000", order.getId());
-        assertEquals(1708560000L, order.getOrderDate());
+        assertEquals(1708560000L, order.getOrderTime());
         assertEquals("Safira Sudrajat", order.getAuthor());
         assertEquals("WAITING_PAYMENT", order.getStatus());
     }
